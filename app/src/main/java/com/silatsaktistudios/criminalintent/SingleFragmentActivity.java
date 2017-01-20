337b7b353fa17d29c11f67c20af62e214551f5a1
James Anderson <silatjedi@gmail.com>
2017-01-20 12:38:33 -0700

    meh

---
+++ app/src/main/java/com/silatsaktistudios/criminalintent/SingleFragmentActivity.java
@@ -0,0 +1,10 @@
+package com.silatsaktistudios.criminalintent;
+
+import android.support.v4.app.FragmentActivity;
+
+/**
+ * Created by SilatJedi on 1/20/17.
+ */
+
+public abstract class SingleFragmentActivity extends FragmentActivity {
+}
